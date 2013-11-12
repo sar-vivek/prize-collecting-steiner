7c7deae3e071671fa25f4f1f29dd8da071c613e6
Vivek Sardeshmukh <vsardeshmukh@r-lnx213.cs.uiowa.edu>
2013-11-12 15:27:41 -0600

    localsearch is complete (provided that compute_cost is already exists

---
+++ compute_cost.m
@@ -0,0 +1,7 @@
+%% Constructs a minimum weight tree which spans all the nodes in _X_ 
+%%Returns the cost of the tree + prizes of nodes that are not in X 
+%%Author : Vivek Sardeshmukh
+function c = compute_cost(G, Prize, X)
+
+
+end
--- localsearch.m
+++ localsearch.m
@@ -1,8 +1,30 @@
+%% Performs first-improvement local search 
+%%Neighborhood definition : X and nX are neighbors if they differ exactly in one node. 
+%%G is edge-weight matrix 
+%%Prize is an array denoting penalties (if we skip) or prize (if we visit) of nodes
+%%Requires : compute_cost function : which constructs a tree spanning nodes in X and returns the cost of tree + penalties of not visited nodes.
+%%Author : Vivek Sardeshmukh
 function X = localsearch(G, Prize, X)
 
-local_improvement = 1
+local_improvement = 1;
+%_c_ is the cost of current solution 
+c=compute_cost(G, Prize, X);
 while(local_improvement)
-    
-
+    local_improvement=0 ;
+    for i=1:length(G(1,:))
+        %_i_ is not in X then _nX_ will consider _i_
+        if (any(X==i))
+            nX= setdiff(X, i);
+        else
+            nX= union(X, i);
+        end
+        nc=compute_cost(G,Prize, nX);
+        if (nc < c)
+            X = nX;
+            c = nc;
+            local_improvement=1;
+            % first improvement found so stop and exit
+        end
+    end
 end
 
