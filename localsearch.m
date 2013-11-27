c4e87705ac104e95d2138f2eabaa699352a0463d
Vivek Sardeshmukh <vsardeshmukh@serv16.divms.uiowa.edu>
2013-11-27 13:11:05 -0600

    localsearch complete

--- localsearch.m
+++ localsearch.m
@@ -4,23 +4,22 @@
 %%Prize is an array denoting penalties (if we skip) or prize (if we visit) of nodes
 %%Requires : compute_cost function : which constructs a tree spanning nodes in X and returns the cost of tree + penalties of not visited nodes.
 %%Author: Vivek Sardeshmukh
-function X = localsearch(G, Prize, X, root)
+function [X,c] = localsearch(G, Prize, X, c,root)
 
 local_improvement = 1;
 %_c_ is the cost of current solution 
-c=compute_cost(G, Prize, X);
 while(local_improvement)
     local_improvement=0 ;
     for i=1:length(G(1,:))
         if(i==root)
             continue;
-        else if (any(X==i)) %_i_ is not in X then _nX_ will consider _i_
+        else if (any(X==i)) %_i_ is in X then _nX_ will not consider _i_
             nX= setdiff(X, i);
-        else if(Prize[i]!=0) %_i_ is a terminal node
+        else 
             nX= union(X, i);
         end
-        nc=compute_cost(G,Prize, nX);
-        if (nc < c)
+        nc=compute_score(G,Prize, nX);
+        if (nc > c)
             X = nX;
             c = nc;
             local_improvement=1;
