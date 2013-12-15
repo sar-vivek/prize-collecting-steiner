3ef98ba380dc0aa6b68f950a0db5493c97ed0514
Vivek Sardeshmukh <vsardeshmukh@serv15.divms.uiowa.edu>
2013-12-15 00:17:25 -0600

    inputfile and output file are arguments to main

--- PCSTMain.m
+++ PCSTMain.m
@@ -2,7 +2,7 @@
 %% Each vertex in V is denoted by the index 1,2,3,... size(V)
 %% Prize is vertex penalites, a list of int
 %% Author : Preethi Issac and Vivek B Sardeshmukh
-function T = PCSTMain(Prize)
+function [scoreX, scoreY, scoreZ] = PCSTMain(inputfile, outputfile)
     global G;
     global Prize;
     global r;
@@ -39,7 +39,7 @@ function T = PCSTMain(Prize)
 
      Prize = [10 0 0 150 200 0 100 0 0 20];
     
-    %[G,Prize] = InputData('C02-A.stp');
+    %[G,Prize] = InputData(inputfile);
     %initial sol
     tic;
     [T, X, scoreX] = InitSol();
@@ -67,8 +67,8 @@ function T = PCSTMain(Prize)
     %display(Z);
     %display(scoreZ);
 
-    fileid = fopen('output.txt', 'a+');
-    fprintf(fileid, '%d (%d)  %d (%d) %d (%d)  %g %g %g\n', scoreX, dualX, scoreY, dualY, scoreZ, dualZ,  initTime, localTime, simTime);
+    fileid = fopen(outputfile, 'a+');
+    fprintf(fileid, '%d (%d)  %d--%d (%d) %d (%d)  %g %g %g\n', scoreX, dualX, scoreY, count, dualY, scoreZ, dualZ,  initTime, localTime, simTime);
     fclose(fileid);
     exit;
 end
